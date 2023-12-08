638487f18bf77e8a1d88db6f535b3da63bae805c
Aryan Kumar <lci2020004@iiitl.ac.in>
2023-12-08 20:46:12 +0530

    add redirect to auth page.

--- src/App.js
+++ src/App.js
@@ -5,20 +5,28 @@ import BlogDetail from "./components/BlogDetail";
 import AddBlog from "./components/AddBlog";
 
 import React, { useEffect } from "react";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation, useNavigate } from "react-router-dom";
 import Auth from "./components/Auth";
 import { useDispatch, useSelector } from "react-redux";
 import { authActions } from "./store";
 function App() {
   const dispath = useDispatch();
+  const navigate = useNavigate();
+  const location = useLocation();
 
   const isLoggedIn = useSelector((state) => state.isLoggedIn);
   console.log(isLoggedIn);
   useEffect(() => {
     if (localStorage.getItem("userId")) {
       dispath(authActions.login());
+    } else {
+      // Redirect to "/auth" if the current route is not "/auth"
+      if (location.pathname !== "/auth") {
+        navigate("/auth");
+      }
     }
-  }, [dispath]);
+  }, [dispath, location.pathname, navigate]);
+  console.log(isLoggedIn);
   return (
     <React.Fragment>
       <header>
